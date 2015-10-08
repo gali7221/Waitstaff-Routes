010e445bfd0717df06d9f69b7fd7b18205c83bcd
gali7221 <gali7221@gmail.com>
2015-10-08 16:50:47 -0400

    successfully added routes

--- wsc-app.js
+++ wsc-app.js
@@ -1,44 +1,61 @@
-/*angular.module('calcController', ['ngMessages', 'ngRoute'])
-	.config(['$routeProvider', function($routeProvider){
+angular.module('WSCApp', ['ngRoute', 'ngAnimate', 'ngMessages'])
+	.config(function($routeProvider){
 		$routeProvider.when('/', {
-			templateUrl: 'home.html', 
-			controller: 'HomeCtrl'
-		}).when('/newmeal', {
+			templateUrl: 'home.html' 
+		}).when('/new-meal', {
 			templateUrl: 'newmeal.html', 
-			controller: 'newMealCtrl'
+			controller: 'newmealCtrl'
 		}).when('/earnings', {
 			templateUrl: 'earnings.html', 
 			controller: 'earningsCtrl'
-		}).
-	}])
-	.controller('HomeCtrl', ['$rootScope', function($rootScope){
-
-	}])
-
-	.controller('earningsCtrl', ['$scope', '$rootScope', function($scope, $rootScope){
-		$scope.staff = $rootScope.staff || 0;
-
-		$scope.init = function(){
-			$scope.staff.meals = 0;
-			$scope.staff.tips = 0;
-			$scope.staff.avg = 0;
-
-			$rootScope.staff = {
-				meals: 0,
-				tips: 0,
-				avg: 0
-			};
-		}
-	}])
-	.controller('newMealCtrl', ['$scope', '$rootScope', '$routeParams', function($scope, $rootScope, $routeParams)]){
-		$scope.customer = $scope.customer || {};
-		$rootScope.staff = $rootScope.staff || {
+		}).otherwise({
+			redirectTo: '/'
+		});
+	})
+	.controller('newmealCtrl', function($scope, $rootScope){
+		$rootScope.earnings = $rootScope.earnings || { // save values
 			meals: 0,
 			tips: 0,
-			avg: 0
+			avgTips: 0
 		};
 
-		$scope.compute = function(){
-			
-		}
-	}*/
+		$scope.submit = function(){
+			if ($scope.mealForm.$valid) {
+				$scope.errorShow = false;
+				$scope.subTotal = $scope.mealPrice * $scope.taxRate/100 + $scope.mealPrice;
+				$scope.tip = $scope.mealPrice * $scope.tipPer/100;
+				$scope.total = $scope.subTotal + $scope.tip;
+				$rootScope.earnings.meals++;
+				$rootScope.earnings.tips += $scope.tip;
+				$rootScope.earnings.avgTips = $rootScope.earnings.tips / $rootScope.earnings.meals;
+			}
+			else {
+				$scope.errorShow = true;
+			}
+		};
+
+		$scope.cancel = function(){
+			delete $scope.mealPrice;
+			delete $scope.taxRate;
+			delete $scope.tipPer;
+			delete $scope.subTotal;
+			delete $scope.tip;
+			delete $scope.total;
+			$rootScope.earnings = {
+				meals: 0, 
+				tips: 0, 
+				avgTips: 0
+			};
+			$scope.errorShow = false;
+		};
+	})
+	.controller('earningsCtrl', function($scope, $rootScope){
+		$scope.reset = function(){
+			$rootScope.earnings = {
+				meals: 0,
+				tips: 0,
+				avgTips: 0
+			}
+		};
+		
+	});
